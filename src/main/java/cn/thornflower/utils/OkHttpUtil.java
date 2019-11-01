a321c0e2e0e798e4ebcd446ca583e70827e2a8d3
2017111501 <2017111501@192.168.101.1>
2019-11-01 16:43:03 +0800

    2019年11月1日16:32:22

---
+++ src/main/java/cn/thornflower/utils/OkHttpUtil.java
@@ -0,0 +1,279 @@
+/**  
+ * Project Name:ThornflowerOCR  
+ * File Name:OkHttpUtil.java  
+ * Package Name:cn.thornflower.utils  
+ * Date:2019年10月30日下午5:06:01  
+ * Copyright (c) 2019
+ *  
+*/  
+  
+package cn.thornflower.utils;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**  
+ * ClassName:OkHttpUtil <br/>  
+ * Function: TODO ADD FUNCTION. <br/>  
+ * Reason:   TODO ADD REASON. <br/>  
+ * Date:     2019年10月30日 下午5:06:01 <br/>  
+ * @author   李景云  
+ * @version    
+ * @since    JDK 1.8  
+ * @see        
+ */
+@Component
+public class OkHttpUtil {
+
+    private static final Logger logger =   LoggerFactory.getLogger(OkHttpUtil.class);
+
+    private static OkHttpClient  okHttpClient;
+
+    @Autowired
+    public OkHttpUtil(OkHttpClient  okHttpClient) {
+        OkHttpUtil.okHttpClient= okHttpClient;
+    } 
+    
+    
+    /**
+     * get
+     * @param url     请求的url
+     * @param queries 请求的参数，在浏览器？后面的数据，没有可以传null
+     * @return
+     */
+    public static  String get(String url, Map<String, String> queries) {
+        String responseBody = "";
+        StringBuffer sb = new StringBuffer(url);
+        if (queries != null && queries.keySet().size() > 0) {
+            boolean firstFlag = true;
+            Iterator iterator = queries.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry<String, String>) iterator.next();
+                if (firstFlag) {
+                    sb.append("?" + entry.getKey() + "=" + entry.getValue());
+                    firstFlag = false;
+                } else {
+                    sb.append("&" + entry.getKey() + "=" + entry.getValue());
+                }
+            }
+        }
+        Request request = new Request.Builder()
+                .url(sb.toString())
+                .build();
+        Response response = null;
+        try { 
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            logger.error("okhttp3 put error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+
+    /**
+     * post
+     *
+     * @param url    请求的url
+     * @param params post form 提交的参数
+     * @return
+     */
+    public static String post(String url, Map<String, String> params) {
+        String responseBody = "";
+        FormBody.Builder builder = new FormBody.Builder();
+        //添加参数
+        if (params != null && params.keySet().size() > 0) {
+            for (String key : params.keySet()) {
+                builder.add(key, params.get(key));
+            }
+        }
+        Request request = new Request.Builder()
+                .url(url)
+                .post(builder.build())
+                .build();
+        Response response = null;
+        try { 
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            logger.error("okhttp3 post error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+
+    /**
+     * get
+     * @param url     请求的url
+     * @param queries 请求的参数，在浏览器？后面的数据，没有可以传null
+     * @return
+     */
+    public static String getForHeader(String url, Map<String, String> queries) {
+        String responseBody = "";
+        StringBuffer sb = new StringBuffer(url);
+        if (queries != null && queries.keySet().size() > 0) {
+            boolean firstFlag = true;
+            Iterator iterator = queries.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = (Map.Entry<String, String>) iterator.next();
+                if (firstFlag) {
+                    sb.append("?" + entry.getKey() + "=" + entry.getValue());
+                    firstFlag = false;
+                } else {
+                    sb.append("&" + entry.getKey() + "=" + entry.getValue());
+                }
+            }
+        }
+        Request request = new Request.Builder()
+                .addHeader("key", "value")
+                .url(sb.toString())
+                .build();
+        Response response = null;
+        try { 
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            logger.error("okhttp3 put error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+
+    /**
+     * Post请求发送JSON数据....{"name":"zhangsan","pwd":"123456"}
+     * 参数一：请求Url
+     * 参数二：请求的JSON
+     * 参数三：请求回调
+     */
+    public static String postJsonParams(String url, String jsonParams) {
+        String responseBody = "";
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonParams);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+        Response response = null;
+        try { 
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            logger.error("okhttp3 post error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+
+    /**
+     * Post请求发送xml数据....
+     * 参数一：请求Url
+     * 参数二：请求的xmlString
+     * 参数三：请求回调
+     */
+    public static String postXmlParams(String url, String xml) {
+        String responseBody = "";
+        RequestBody requestBody = RequestBody.create(MediaType.parse("application/xml; charset=utf-8"), xml);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+        Response response = null;
+        try { 
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            logger.error("okhttp3 post error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return responseBody;
+    }
+    
+    /**
+     * Post请求发送 文件...
+     * 参数一：请求Url
+     * 参数二：请求的file
+     * 参数三：请求参数map
+     */
+    public static String postFile(String url, Map<String, Object> map ,File file) {
+        // form 表单形式上传
+        MultipartBody.Builder requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM);
+        if(file != null){
+            // MediaType.parse() 里面是上传的文件类型。
+            RequestBody body = RequestBody.create(MediaType.parse("image/*"), file);
+            String filename = file.getName();
+            // 参数分别为， 请求key ，文件名称 ， RequestBody
+            requestBody.addFormDataPart("img", file.getName(), body);
+        }
+        if (map != null) {
+            // map 里面是请求中所需要的 key 和 value
+            for (Map.Entry entry : map.entrySet()) {
+                requestBody.addFormDataPart(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+            }
+        }
+        
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody.build())
+                .build();
+        Response response = null;
+        try { 
+            response = okHttpClient.newCall(request).execute();
+            int status = response.code();
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (Exception e) {
+            logger.error("okhttp3 post error >> ex = {}", ExceptionUtils.getStackTrace(e));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return url;
+    }
+}
+  
